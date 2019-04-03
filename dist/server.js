6e16e441a77b86ec4d2585833d43debfc1278c9e
loltisha <alyahadiem@gmail.com>
2019-04-03 20:50:46 +0300

    cors :)

--- dist/server.js
+++ dist/server.js
@@ -66,9 +66,9 @@ var app = (0, _express2.default)();
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 app.use((0, _cors2.default)());
-app.use(function (req, res, next) {
+app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
--- src/server.js
+++ src/server.js
@@ -23,10 +23,10 @@ const app = express();
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.all('*', function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "X-Requested-With");
+ next();
 });
 
 // define port for API to run on
